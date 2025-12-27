0e85b41846e745274f5ae7e81ea32493cb450dbe
__deesh__ <jagadeeswarareddy.peram@devin.tech>
2025-12-27 15:25:22 +0530

    feat: Add new Svelte store for connection form state management and action buttons to database forms.

--- src/lib/components/datasource/DataSource.svelte
+++ src/lib/components/datasource/DataSource.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
   import DataSourceSidebar from "$lib/components/datasource/DataSourceSidebar.svelte";
   import ConnectionForm from "$lib/components/datasource/ConnectionForm.svelte";
-  import Button from "$lib/components/Button.svelte";
   import { type Driver, drivers } from "$lib/components/datasource/DriverList";
   import { connectionForm } from "$lib/components/datasource/connectionStore.svelte";
-  import { getCurrentWindow } from "@tauri-apps/api/window";
   import { onDestroy } from "svelte";
 
   let selectedDriver = $state<Driver | null>(drivers[0]);
@@ -18,38 +16,25 @@
     connectionForm.setDriver(selectedDriver);
   });
 
-  async function handleClose() {
-    const window = getCurrentWindow();
-    await window.close();
-  }
-
   onDestroy(() => {
     connectionForm.reset();
   });
 </script>
 
 <div
-    class="flex flex-col w-full h-full bg-[--theme-bg-primary] overflow-hidden"
+  class="flex flex-col w-full h-full bg-[--theme-bg-primary] overflow-hidden"
 >
-    <div class="flex grow w-full overflow-hidden">
-        <!-- Sidebar -->
-        <div
-            class="max-w-60 w-full shrink-0 h-full border-r border-(--theme-border-default)"
-        >
-            <DataSourceSidebar onSelect={handleDriverSelect} {selectedDriver} />
-        </div>
-
-        <!-- Main Content -->
-        <div class="grow h-full bg-[--theme-bg-primary] min-w-0">
-          <ConnectionForm driver={selectedDriver} />
-        </div>
+  <div class="flex grow w-full overflow-hidden">
+    <!-- Sidebar -->
+    <div
+      class="max-w-60 w-full shrink-0 h-full border-r border-(--theme-border-default)"
+    >
+      <DataSourceSidebar onSelect={handleDriverSelect} {selectedDriver} />
     </div>
 
-    <div class="flex justify-center items-center py-2 px-6 mb-3 w-full">
-        <div class="flex space-x-3">
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button>Apply</Button>
-            <Button>Test Connection</Button>
-        </div>
+    <!-- Main Content -->
+    <div class="grow h-full bg-[--theme-bg-primary] min-w-0">
+      <ConnectionForm driver={selectedDriver} />
     </div>
+  </div>
 </div>
