b508e6561f139d936aab8bcb6c00a06f9ccc4955
__deesh__ <jagadeeswarareddy.peram@devin.tech>
2025-12-29 22:31:54 +0530

    feat: add custom SVG icons for toaster notifications and integrate them into the layout.

---
+++ src/lib/svg/ErrorMark.svelte
@@ -0,0 +1,16 @@
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1.6em"
+    height="1.6em"
+    viewBox="0 0 48 48"
+    ><title xmlns="">error</title><path
+        fill="#ef4444"
+        fill-rule="evenodd"
+        stroke="#ef4444"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="4"
+        d="m6 11l5-5l13 13L37 6l5 5l-13 13l13 13l-5 5l-13-13l-13 13l-5-5l13-13z"
+        clip-rule="evenodd"
+    /></svg
+>
