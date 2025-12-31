cd0def834873919e2dce05ab4ef864510f7b5376
__deesh__ <jagadeeswarareddy.peram@devin.tech>
2025-12-31 22:12:55 +0530

    feat: add FileTree component and integrate it into the main page and a new dedicated test page.

---
+++ src/routes/tree-test/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    import FileTree from "$lib/components/explorer/FileTree.svelte";
+
+    const data = [
+        {
+            name: "src",
+            type: "folder",
+            children: [
+                {
+                    name: "routes",
+                    type: "folder",
+                    children: [
+                        {
+                            name: "demo",
+                            type: "folder",
+                            children: [{ name: "+page.svelte", type: "file" }],
+                        },
+                    ],
+                },
+                {
+                    name: "lib",
+                    type: "folder",
+                    children: [
+                        {
+                            name: "components",
+                            type: "folder",
+                            children: [{ name: "Tree.svelte", type: "file" }],
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            name: "database",
+            type: "folder",
+            children: [
+                { name: "themes.db", type: "file" },
+                {
+                    name: "keys",
+                    type: "folder",
+                    children: [{ name: "api.key", type: "key" }],
+                },
+            ],
+        },
+    ];
+</script>
+
+<div class="p-8 w-full max-w-sm border h-full bg-background text-foreground">
+    <h2 class="mb-4 font-bold">Tree Test</h2>
+    <FileTree items={data} />
+</div>
